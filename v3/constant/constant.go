ff79477d079d08af37940087c732afdd0253e270
Ezra Rizkiatama <erizkiatama@gmail.com>
2022-12-12 14:14:51 +0800

    feat(selfcare-category): fix map category table name

--- v3/constant/constant.go
+++ v3/constant/constant.go
@@ -4,6 +4,7 @@ var (
 	MAP_CATEGORY_TABLENAME map[string]string = map[string]string{
 		"Sinema":             `"selfcare".movies`,
 		"Latihan Pernafasan": `"selfcare".breathing_exercises`,
+		"Audio Self-Healing": `"selfcare".self_healing_audios`,
 	}
 	CATEGORY_BREATHING_EXERCISE_ID = 2
 	CATEGORY_AUDIO_SELF_HEALING_ID = 3
