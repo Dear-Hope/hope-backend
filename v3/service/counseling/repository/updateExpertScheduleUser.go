3c312ac3ab50a2dcfe76ce37200b1697e68647be
Ezra Rizkiatama <erizkiatama@gmail.com>
2023-02-14 16:02:42 +0800

    hotfix booking query

--- v3/service/counseling/repository/updateExpertScheduleUser.go
+++ v3/service/counseling/repository/updateExpertScheduleUser.go
@@ -7,14 +7,18 @@ import (
 func (ths *repository) UpdateExpertScheduleUser(consulId, userId int64) (bool, error) {
 	res, err := ths.db.Exec(
 		ths.db.Rebind(
-			`WITH consul AS (
-				UPDATE "counseling".consultations SET user_id = ?
-				FROM "counseling".consultations c JOIN "counseling".expert_schedules es
-				ON c.schedule_id = es.id WHERE c.id = ? AND es.is_booked = false RETURNING c.schedule_id
+			`WITH row AS (
+    			SELECT es.is_booked
+    			FROM "counseling".expert_schedules es, "counseling".consultations c
+    			WHERE c.schedule_id = es.id AND c.id = ?
+			), consul AS (
+    			UPDATE "counseling".consultations SET user_id = ?
+    			WHERE id = ? AND (SELECT is_booked FROM row) = false
+    			RETURNING schedule_id
 			)
-			UPDATE "counseling".expert_schedules SET is_booked = true WHERE id = (SELECT schedule_id FROM consul LIMIT 1)`,
+			UPDATE "counseling".expert_schedules SET is_booked = true WHERE id = (SELECT schedule_id FROM consul LIMIT 1);`,
 		),
-		userId, consulId,
+		consulId, userId, consulId,
 	)
 	if err != nil {
 		log.Printf("expert schedule update user failed: %s", err.Error())
